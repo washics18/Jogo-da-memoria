dfa1d955d1ba2784bfc35f93188daf84a2d22849
washics18 <washics18@hotmail.com>
2021-08-13 11:38:39 -0300

    Onclick no cartão para frente e verso

--- SRC/components/memory-card/index.js
+++ SRC/components/memory-card/index.js
@@ -1,17 +1,101 @@
 
-const createMemoryCard = ({src , alt , className}) =>  `
+const memoryCard = () =>  { 
 
-<article class="memory-card ${className}">
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+    
+    $style.textContent = `
+
+.memory-card {
+
+width: 155px;
+height: 155px;
+position : relative;
+
+}
+    .memory-card .card{
+    width: 100%;
+    height: 100%; 
+    background-color: #f25a70 ;
+    border-radius: 30px;
+    display: flex;
+    justify-content: center ;
+    align-items: center;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+    cursor: pointer;
+    position : absolute;
+    
+    }
+
+    .memory-card.-active .card {
+        display : none;
+    }
+
+    .memory-card.-active .card.-front{
+        display: flex;
+    }
+    
+    .memory-card .card.-front{
+    background-color: #fff;
+  
+    }
+    
+    .memory-card .card.-front::before{
+    content: "";
+    width: 95px;
+    height: 95px;
+    background-color: #d4d4d4;
+    border-radius : 50px;
+    position: absolute;
+        
+    }
+    
+    .card > .icon {
+    width: 100px;
+    height: 100px;
+    
+    }
+    
+    .memory-card .card.-front > .icon {
+    position : absolute;
+    transform: translateY(-12px);
+    }
+    
+    `;
+    
+    $head.insertBefore($style , null);
+    
+
+return ({src , alt , className}) => `
+
+<div class="memory-card -active" onclick = "handleClick(this)">
+<article class="card -front"> 
 <img 
 src= "${src}"
 alt= "${alt}" 
 class='icon'
-onclick = handleClick();
+
 />
 
 </article>
 
+<article class="card">
+<img 
+src= "img/icon-collabcode.png"
+alt= "Gueio Mascote Collabcode" 
+class='icon'
+
+/>
+
+</article>
+</div>
 `;
 
+};
 
-const handleClick = () => console.log("foi");
+
+
+const handleClick = $component => {
+    
+    $component.classList.toggle("-active");
+}
--- SRC/pages/game/index.js
+++ SRC/pages/game/index.js
@@ -2,31 +2,31 @@ const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
 
+const createMemoryCard = memoryCard();
 
 const $memoryCardC = createMemoryCard({ 
-className: "-front",
+
 src: "img/icon-c.png" ,
 alt: 'Icone livro de C++ '});
 
 const $memoryCardJS = createMemoryCard({ 
-className: "-front" , 
+
 src: "img/icon-js.png" ,  
 alt: 'Icone livro de JS ' });
 
 const $memoryCardJava = createMemoryCard({
-className: "-front" , 
+
 src: "img/icon-java.png" , 
 alt: 'Icone livro de Java'});
 
 const $memoryCard = createMemoryCard({ 
 
-className: "" ,
+
 src: "img/icon-collabcode.png" , 
 alt: 'Gueio Mascote Collabcode' });
 
 const $memoryCardWoman = createMemoryCard({ 
 
-className: "-front" , 
 src: "img/icon-woman.png" , 
 alt: 'Icone woman codando'});
 
